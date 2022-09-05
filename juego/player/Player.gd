4f1186b7ba909363f8744c4f8527b5e833a78ea3
AgustinAnriquez <agustin.anriquez98@gmail.com>
2022-09-05 13:09:50 -0300

    se agregan animaciones a player

--- juego/player/Player.gd
+++ juego/player/Player.gd
@@ -2,7 +2,11 @@
 class_name Godotin
 extends KinematicBody
 
+## Constantes
 const direccion_arriba:Vector3 = Vector3.UP
+
+## Enums
+enum {SUELO, AIRE}
 
 export var velocidad_max: Vector2 = Vector2(10.0, 60.0)
 export var gravedad: float = 9.8
@@ -14,9 +18,11 @@ var movimiento: Vector3 = Vector3.ZERO
 var vector_snap: Vector3 = Vector3.DOWN
 var salto_interrumpido = false
 var saltando = false
+var cayendo = false
 
 onready var brazo_camara: SpringArm = $BrazoCamara
 onready var armadura: Spatial = $Armadura
+onready var arbol_animacion: ArbolAnimacionPlayer = $ArbolDeAnimacion
 
 ## Metodos
 func _process(delta: float) -> void:
@@ -29,8 +35,14 @@ func _physics_process(delta: float) -> void:
 
 	var direccion_vista_player = Vector2(movimiento.z, movimiento.x)
 	if direccion_vista_player.length() > 0:
-		print("entra aca")
 		armadura.rotation.y = direccion_vista_player.angle()
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("disparar"):
+		arbol_animacion.set_mezcla_disparar(1)
+	elif event.is_action_released("disparar"):
+		arbol_animacion.set_mezcla_disparar(0)
+
 
 ## Metodos custom
 func movimiento_horizontal() -> void:
@@ -41,6 +53,7 @@ func tomar_direccion() -> Vector3:
 	var direccion: Vector3 = Vector3.ZERO
 	direccion.x = Input.get_action_strength("mov_derecha") - Input.get_action_strength("mov_izquierda")
 	direccion.z = Input.get_action_strength("mov_atras") - Input.get_action_strength("mov_adelante")
+	arbol_animacion.set_valor_mezcla_idle_caminar(direccion.length())
 
 	direccion = direccion.rotated(Vector3.UP, brazo_camara.rotation.y).normalized()
 	
@@ -59,11 +72,15 @@ func movimiento_vertical() -> void:
 	var inicio_salto: bool = is_on_floor() and Input.is_action_just_pressed("saltar")
 	
 	if inicio_salto:
+		arbol_animacion.set_transicion_suelo_aire(AIRE)
+		arbol_animacion.set_mezcla_saltar_caer(0)
 		vector_snap = Vector3.ZERO
 		saltando = true
 		salto_interrumpido = false
+		cayendo = false
 
 	elif tocando_suelo:
+		arbol_animacion.set_transicion_suelo_aire(SUELO)
 		vector_snap = Vector3.DOWN
 
 	if movimiento.y >= velocidad_max.y:
@@ -71,3 +88,8 @@ func movimiento_vertical() -> void:
 
 	if Input.is_action_just_pressed("saltar") and saltando and not salto_interrumpido:
 		movimiento.y += fuerza_salto
+
+	if movimiento.y <= 0 and not cayendo:
+		cayendo = true
+		for i in range(1, 11, 1):
+			arbol_animacion.set_mezcla_saltar_caer(i* 0.1)
